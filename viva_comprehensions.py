796f4533f7faaa1538c5ea395ff3e27577089102
Vandana <vandana@Zipcoders-MBP.fios-router.home>
2022-03-22 00:05:17 -0400

    dict_compre finished

--- viva_comprehensions.py
+++ viva_comprehensions.py
@@ -7,6 +7,7 @@ class Parity(enum.Enum):
     EVEN = 1
 
 
+
 def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
     """
     Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
@@ -18,8 +19,13 @@ def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
     :param parity:
     :return:
     """
-    pass
+   # if parity is parity.EVEN:
+   #     res=[x for x in range(start,stop) if x%2==0]
+   # else:
+    #    res=[x for x in range(start,stop) if x%2==1]
+   #return res
 
+    return [x for x in range(start, stop) if (parity == parity.EVEN  and x % 2 == 0) or (parity == parity.ODD and x % 2 !=0)]
 
 def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
     """
@@ -33,7 +39,17 @@ def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
     :param strategy:
     :return:
     """
-    pass
+    #list_comp=[value for value in range(start, stop) ]
+    #print(list_comp)
+    #dict= {x:strategy(x) for x in list_comp }
+    #return dict
+
+    return { x:strategy(x) for x in range(start,stop)}
+
+
+
+
+
 
 
 def gen_set(val_in: str) -> Set:
@@ -45,4 +61,9 @@ def gen_set(val_in: str) -> Set:
     :param val_in:
     :return:
     """
-    pass
+    dict_comp={val_in[x].upper() for x in range(0,len(val_in)) if val_in[x].islower()}
+    list(dict_comp).sort()
+    return set(dict_comp)
+
+    #return{x.upper() for x in val_in if x==x.lower()}
+
